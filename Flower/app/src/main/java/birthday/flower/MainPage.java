108172082750f74bb10936b0068ec8fca66a5127
Brandon Haskett <brhaskett@yahoo.com>
2016-09-17 23:07:48 -0400

    setting up a simple on click listener
    doesn't do much

--- Flower/app/src/main/java/birthday/flower/MainPage.java
+++ Flower/app/src/main/java/birthday/flower/MainPage.java
@@ -3,8 +3,12 @@ package birthday.flower;
 import android.database.SQLException;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import java.io.IOException;
 import java.util.List;
@@ -40,5 +44,14 @@ public class MainPage extends AppCompatActivity
         ArrayAdapter adapter = new ArrayAdapter<String>(this, R.layout.activity_listview, flowers);
         ListView listView= (ListView) findViewById(R.id.names);
         listView.setAdapter(adapter);
+
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                String herb = ((TextView)view).getText().toString();
+
+                Toast.makeText(MainPage.this, "the clicked item had " + herb + " as the text", Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
